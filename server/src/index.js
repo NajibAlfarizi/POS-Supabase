3c4d24b7300106b5fe46ee36c2dc2e85dfee3d57
Najib Alfarizi <najibalfarizi5@gmail.com>
2025-08-26 18:00:45 +0700

    feat: add Winston logging for improved request tracking and server logging

--- server/src/index.js
+++ server/src/index.js
@@ -3,17 +3,21 @@ dotenv.config();
 
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
+import logger from './config/logger.js';
 
 const app = express();
 app.use(express.json());
 
-// Register auth routes
-app.use('/auth', authRoutes);
-app.get('/', (req, res) => {
-  res.send('POS Supabase Express API is running');
+// Middleware logging dengan Winston
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.originalUrl}`);
+  next();
 });
 
-const PORT = process.env.PORT || 5000;
+// Register auth routes
+app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
