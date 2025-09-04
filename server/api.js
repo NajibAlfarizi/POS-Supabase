8978e3604db98b867d089a0b1dd635ae65e3030e
Najib Alfarizi <najibalfarizi5@gmail.com>
2025-09-04 16:01:25 +0700

    refactor: comment out unused route imports and reorder middleware for clarity

--- server/api.js
+++ server/api.js
@@ -4,15 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import authRoutes from './src/routes/authRoutes.js';
-// import merekRoutes from './src/routes/merekRoutes.js';
-// import kategoriBarangRoutes from './src/routes/kategoriBarangRoutes.js';
-// import sparepartRoutes from './src/routes/sparepartRoutes.js';
-// import transaksiRoutes from './src/routes/transaksiRoutes.js';
-// import laporanRoutes from './src/routes/laporanRoutes.js';
-
 const app = express();
-app.use(express.json());
 app.use(
     cors({
         origin: true,
@@ -27,8 +19,12 @@ app.use(
         ],
     })
 );
+
 app.get('/', (req, res) => {
   res.json({ message: 'API is working!' });
 });
+
+// Body parser hanya untuk route POST/PUT
+app.use(express.json());
 
 export default serverless(app);
